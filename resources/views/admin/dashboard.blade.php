62d371c6ba46f04b7256ae638850880a90d1ba73
Anderson <mpiscoyaaq.13@gmail.com>
2026-01-08 11:23:21 -0500

    Proyecto Laravel v10 inicial

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a tu panel AdminLTE en Laravel 10.</p>
+@stop
